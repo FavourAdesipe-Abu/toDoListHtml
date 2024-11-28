f655a8b38934ed61180b1dcd7e368ebb8abcc524
Rito.0 <113887008+FavourAdesipe-Abu@users.noreply.github.com>
2024-11-28 20:28:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -38,11 +38,11 @@ function addItem(e) {
             <div class="btn-container">
               <!-- edit btn -->
               <button type="button" class="edit-btn">
-                <i class="fas fa-edit"></i>
+                <i class="fa-solid fa-edit"></i>
               </button>
               <!-- delete btn -->
               <button type="button" class="delete-btn">
-                <i class="fas fa-trash"></i>
+                <i class="fa-solid fa-trash"></i>
               </button>
             </div>
           `;
